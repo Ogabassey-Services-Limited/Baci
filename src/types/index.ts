6f29bf3453c9defe5d2cc5354bba34c60f6bd6ef
Bassey John <basseybjohn@gmail.com>
2025-11-01 16:07:38 +0000

    The `npm install` command failed in my project. Analyze the following er

---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+
+export interface BrandColors {
+  primary: string;
+  secondary: string;
+  accent: string;
+  background: string;
+  text: string;
+}
+
+export interface SampleProduct {
+  name: string;
+  price: string;
+  imageKeyword: string;
+  description: string;
+  imageBase64: string;
+}
